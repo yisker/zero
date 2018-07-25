8f0889630f0d70c3437cb39370f438d69036f96b
yuan1821 <yuan1821@163.com>
2018-07-25 13:18:05 +0800

    增加了调试按钮

--- mage/mage_forst.lua
+++ mage/mage_forst.lua
@@ -179,16 +179,13 @@ do
     --   end
 end
 
-
-
-
 ----------------------------------------------
 -- 模块属性
 -----------------------------------------------------------
 -- 定义循环的唯一ID，可以去https://1024tools.com/uuid生成，保证每次都不一样，宇宙唯一。
 local rotation_id = "0c02decd-b730-4509-b5bf-259d28ca3c28";
 -- 定义循环的英文名称。
-local rotation_name = "test";
+local rotation_name = "bing_fa";
 Scorpio(rotation_name)("");
 import "zeus";
 -- 定义多语言字符串。
@@ -207,20 +204,21 @@ if L then
     L["aoetg"] = "暴风雪技能目标";
     L["aoenum"] = "进入AOE循环人数";
     L["targets"] = "目标选择";
+    L["ydebug"] = "调试信息";
 end
-L = _Locale("zhTW");
-if L then
-    -- 繁体中文系列。
-    L[rotation_name] = "測試";
-    L["Welcome to use test module."] = "歡迎使用測試模組！";
-    L["Test 1"] = "測試1號";
-    L["Test 2"] = "測試2號";
-    L["At least 2 values must be entered."] = "至少要输入2个值";
-    L["Test Category"] = "測試類別";
-    L["Test 3"] = "測試3號";
-    L["Test 4"] = "測試4號";
-    L["Test 5"] = "測試5號";
-end
+-- L = _Locale("zhTW");
+-- if L then
+--     -- 繁体中文系列。
+--     L[rotation_name] = "測試";
+--     L["Welcome to use test module."] = "歡迎使用測試模組！";
+--     L["Test 1"] = "測試1號";
+--     L["Test 2"] = "測試2號";
+--     L["At least 2 values must be entered."] = "至少要输入2个值";
+--     L["Test Category"] = "測試類別";
+--     L["Test 3"] = "測試3號";
+--     L["Test 4"] = "測試4號";
+--     L["Test 5"] = "測試5號";
+-- end
 L = _Locale;
 local rotation = zeus.rotation(rotation_id, L[rotation_name]);
 -- 定义循环加载并可用时的消息，填入"N/A"则不显示。
@@ -230,7 +228,7 @@ rotation.condition_no_message = "N/A";
 -- 定义循环的执行间隔（秒），如果不设默认是0.1。
 rotation.interval = 0.1;
 -- 定义模块专用宏命令，下面的例子会定义出：/zeus test [argument]。如果不需要宏控制，则删除下面一行。
-rotation.macro = "tt";
+rotation.macro = "bingfa";
 -----------------------------------------------------------
 -- 模块变量
 -----------------------------------------------------------
@@ -239,127 +237,69 @@ rotation.macro = "tt";
 -- 类别和变量会根据添加的先后顺序，依次在界面自上而下显示。默认类别下的变量会显示在最上面。
 -- 变量的当前值会保存在WTF中。
 -----------------------------------------------------------
-
-local aoetg_setting = rotation.default_setting_category:create_setting("aoetg"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
-aoetg_setting.display_name = L["aoetg"];
-aoetg_setting.description = "用来确定暴风雪技能目标释放位置，智能表示放在人群最密集的地方"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
-aoetg_setting.value_type = rotation_setting_type.text; -- 变量值类型（text类型）
-aoetg_setting.default_value = "智能"; -- 变量默认值
-aoetg_setting.optional_values = {"智能", "自己", "小队T", "焦点", "鼠标", "当前目标"}; -- 变量备选值（设置备选值后会出现单选下拉菜单，供用户选择）
-aoetg_setting.can_enable_disable = false; -- 是否支持启用停用（支持则在界面上出现勾选框）
-aoetg_setting.is_enabled_by_default = false; -- 是否默认启用
-aoetg_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（因为带备选值，所以不可能需要校验，不设即可）
-aoetg_setting.value_width = 130; -- 值显示宽度像素（默认为100）
-
-
-local aoenum_setting = rotation.default_setting_category:create_setting("aoenum"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
-aoenum_setting.display_name = L["aoenum"];
-aoenum_setting.description = "超过设定人数，进入AOE循环"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
-aoenum_setting.value_type = rotation_setting_type.number; -- 变量值类型（number类型）
-aoenum_setting.default_value = 3; -- 变量默认值
-aoenum_setting.optional_values = nil; -- 变量备选值（此处不设，则为文本输入框）
-aoenum_setting.can_enable_disable = false; -- 是否支持启用停用（支持则在界面上出现勾选框）
-aoenum_setting.is_enabled_by_default = false; -- 是否默认启用
-aoenum_setting.validator = function(self, value) -- 变量值校验函数，检测值除了类型以外的其他合法性（如果合法就返回true，否则返回false, [错误信息]）
-    if (value > 0) then
-        return true;
-    else
-        return false, "The number is too small.";
-    end
-end;
-aoenum_setting.value_width = 100; -- 值显示宽度像素（默认为100）
+do
+    local aoetg_setting = rotation.default_setting_category:create_setting("aoetg"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
+    aoetg_setting.display_name = L["aoetg"];
+    aoetg_setting.description = "用来确定暴风雪技能目标释放位置，智能表示放在人群最密集的地方"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
+    aoetg_setting.value_type = rotation_setting_type.text; -- 变量值类型（text类型）
+    aoetg_setting.default_value = "智能"; -- 变量默认值
+    aoetg_setting.optional_values = {"智能", "自己", "小队T", "焦点", "鼠标", "当前目标"}; -- 变量备选值（设置备选值后会出现单选下拉菜单，供用户选择）
+    aoetg_setting.can_enable_disable = false; -- 是否支持启用停用（支持则在界面上出现勾选框）
+    aoetg_setting.is_enabled_by_default = false; -- 是否默认启用
+    aoetg_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（因为带备选值，所以不可能需要校验，不设即可）
+    aoetg_setting.value_width = 130; -- 值显示宽度像素（默认为100）
 
 
--- 在类别test_category下添加配置变量test5
-local targets_setting = rotation.default_setting_category:create_setting("targets"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
-targets_setting.display_name = L["targets"];
-targets_setting.description = "确定目标的选择方式"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
-targets_setting.value_type = rotation_setting_type.text; -- 变量值类型（text类型）
-targets_setting.default_value = "智能"; -- 变量默认值
-targets_setting.optional_values = {"智能", "当前目标"}; -- 变量备选值（设置备选值后会出现单选下拉菜单，供用户选择）
-targets_setting.can_enable_disable = false; -- 是否支持启用停用（支持则在界面上出现勾选框）
-targets_setting.is_enabled_by_default = false; -- 是否默认启用
-targets_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（因为带备选值，所以不可能需要校验，不设即可）
-targets_setting.value_width = 130; -- 值显示宽度像素（默认为100）
+    local aoenum_setting = rotation.default_setting_category:create_setting("aoenum"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
+    aoenum_setting.display_name = L["aoenum"];
+    aoenum_setting.description = "超过设定人数，进入AOE循环"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
+    aoenum_setting.value_type = rotation_setting_type.number; -- 变量值类型（number类型）
+    aoenum_setting.default_value = 3; -- 变量默认值
+    aoenum_setting.optional_values = nil; -- 变量备选值（此处不设，则为文本输入框）
+    aoenum_setting.can_enable_disable = false; -- 是否支持启用停用（支持则在界面上出现勾选框）
+    aoenum_setting.is_enabled_by_default = false; -- 是否默认启用
+    aoenum_setting.validator = function(self, value) -- 变量值校验函数，检测值除了类型以外的其他合法性（如果合法就返回true，否则返回false, [错误信息]）
+        if (value > 0) then
+            return true;
+        else
+            return false, "The number is too small.";
+        end
+    end;
+    aoenum_setting.value_width = 100; -- 值显示宽度像素（默认为100）
 
 
--- -- 给默认类别添加一个配置变量test1，并配置相关属性。
-local ydebug_setting = rotation.default_setting_category:create_setting("ydebug"); -- 指定变量的名字为test1，用于在脚本中进行引用
-ydebug_setting.display_name = L["Test 1"]; -- 变量在界面上显示的名字
-ydebug_setting.description = "这是number数组类型的变量，其值为number数组。"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
-ydebug_setting.value_type = rotation_setting_type.plain; -- 变量值类型（number数组类型）
-ydebug_setting.default_value = nil; -- 变量默认值（删除此行不设，则为{}）
-ydebug_setting.optional_values = nil; -- 变量备选值（设置备选值后会出现多选下拉菜单，供用户选择）
-ydebug_setting.can_enable_disable = true; -- 是否支持启用停用（支持则在界面上出现勾选框）
-ydebug_setting.is_enabled_by_default = false; -- 是否默认启用（勾选框默认选中）
-ydebug_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（因为带备选值，所以不可能需要校验，不设即可）
-ydebug_setting.value_width = 120; -- 值显示宽度像素（默认为100）
--- -- 给默认类别添加一个配置变量test2，并配置相关属性。
--- local test2_setting = rotation.default_setting_category:create_setting("test2"); -- 指定变量的名字为test2，用于在脚本中进行引用（名字不能与test1重复）
--- test2_setting.display_name = L["Test 2"]; -- 变量在界面上显示的名字
--- test2_setting.description = "这是text数组类型的变量，其值为string数组。"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
--- test2_setting.value_type = rotation_setting_type.texts; -- 变量值类型（text数组类型）
--- test2_setting.default_value = {"Hello", "World"}; -- 变量默认值（可以留空则为{}）
--- test2_setting.optional_values = nil; -- 变量备选值（此处不设，则为文本输入框，需用户手动输入多个值，用英文逗号分隔）
--- test2_setting.can_enable_disable = false; -- 是否支持启用停用（不支持则在界面上不显示勾选框）
--- test2_setting.is_enabled_by_default = false; -- 是否默认启用
--- test2_setting.validator = function(self, value) -- 变量值校验函数，检测值除了类型以外的其他合法性（如果合法就返回true，否则返回false, [错误信息]）
---     if (#value > 1) then
---         return true;
---     else
---         return false, L["At least 2 values must be entered."];
---     end
--- end;
--- test2_setting.value_width = 90; -- 值显示宽度像素（默认为100）
--- -- 添加一个自定义类别test_category。
--- local test_category = rotation:create_setting_category("test_category"); -- 指定类别的名字，目前没啥用，但是还是写上吧
--- test_category.display_name = L["Test Category"]; -- 类别在界面上显示的名字
--- -- 在类别test_category下添加配置变量test3
--- local test3_setting = test_category:create_setting("test3"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
--- test3_setting.display_name = L["Test 3"];
--- test3_setting.description = "这是plain类型的变量，它没有值。"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
--- test3_setting.value_type = rotation_setting_type.plain; -- 变量值类型（空类型，也就是说只有启用停用一说）
--- test3_setting.default_value = nil; -- 变量默认值（空类型不需要默认值）
--- test3_setting.optional_values = nil; -- 变量备选值（空类型不需要备选值）
--- test3_setting.can_enable_disable = true; -- 是否支持启用停用（既然是空类型就必须支持了，否则还有什么意义）
--- test3_setting.is_enabled_by_default = false; -- 是否默认启用
--- test3_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（空类型连值都没，还验证个毛）
--- test3_setting.value_width = 100; -- 值显示宽度像素（空类型设多少都一回事，反正不显示，就这么搁着吧）
--- -- 在类别test_category下添加配置变量test4
--- local test4_setting = test_category:create_setting("test4"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
--- test4_setting.display_name = L["Test 4"];
--- test4_setting.description = "这是number类型的变量，它只能接受number。"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
--- test4_setting.value_type = rotation_setting_type.number; -- 变量值类型（number类型）
--- test4_setting.default_value = 500; -- 变量默认值
--- test4_setting.optional_values = nil; -- 变量备选值（此处不设，则为文本输入框）
--- test4_setting.can_enable_disable = true; -- 是否支持启用停用（支持则在界面上出现勾选框）
--- test4_setting.is_enabled_by_default = false; -- 是否默认启用
--- test4_setting.validator = function(self, value) -- 变量值校验函数，检测值除了类型以外的其他合法性（如果合法就返回true，否则返回false, [错误信息]）
---     if (value > 100) then
---         return true;
---     else
---         return false, "The number is too small.";
---     end
--- end;
--- test4_setting.value_width = 80; -- 值显示宽度像素（默认为100）
--- -- 在类别test_category下添加配置变量test5
--- local test5_setting = test_category:create_setting("test5"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
--- test5_setting.display_name = L["Test 5"];
--- test5_setting.description = "这是number类型的变量，它只能接受string。"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
--- test5_setting.value_type = rotation_setting_type.text; -- 变量值类型（text类型）
--- test5_setting.default_value = "Hello"; -- 变量默认值
--- test5_setting.optional_values = {"Hello", "World", "Hello, World"}; -- 变量备选值（设置备选值后会出现单选下拉菜单，供用户选择）
--- test5_setting.can_enable_disable = true; -- 是否支持启用停用（支持则在界面上出现勾选框）
--- test5_setting.is_enabled_by_default = false; -- 是否默认启用
--- test5_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（因为带备选值，所以不可能需要校验，不设即可）
--- test5_setting.value_width = 130; -- 值显示宽度像素（默认为100）
+    -- 在类别test_category下添加配置变量test5
+    local targets_setting = rotation.default_setting_category:create_setting("targets"); -- 指定变量的名字，用于在脚本中进行引用（注意，哪怕是不同类别下的配置变量名字也不能重复）
+    targets_setting.display_name = L["targets"];
+    targets_setting.description = "确定目标的选择方式"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
+    targets_setting.value_type = rotation_setting_type.text; -- 变量值类型（text类型）
+    targets_setting.default_value = "智能"; -- 变量默认值
+    targets_setting.optional_values = {"智能", "当前目标"}; -- 变量备选值（设置备选值后会出现单选下拉菜单，供用户选择）
+    targets_setting.can_enable_disable = false; -- 是否支持启用停用（支持则在界面上出现勾选框）
+    targets_setting.is_enabled_by_default = false; -- 是否默认启用
+    targets_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（因为带备选值，所以不可能需要校验，不设即可）
+    targets_setting.value_width = 130; -- 值显示宽度像素（默认为100）
+
+
+    -- -- 给默认类别添加一个配置变量test1，并配置相关属性。
+    local ydebug_setting = rotation.default_setting_category:create_setting("ydebug"); -- 指定变量的名字为test1，用于在脚本中进行引用
+    ydebug_setting.display_name = L["ydebug"]; -- 变量在界面上显示的名字
+    ydebug_setting.description = "这是number数组类型的变量，其值为number数组。"; -- 变量在界面上的鼠标提示说明，充分利用换行符和暴雪颜色可以实现丰富的效果
+    ydebug_setting.value_type = rotation_setting_type.plain; -- 变量值类型（number数组类型）
+    ydebug_setting.default_value = nil; -- 变量默认值（删除此行不设，则为{}）
+    ydebug_setting.optional_values = nil; -- 变量备选值（设置备选值后会出现多选下拉菜单，供用户选择）
+    ydebug_setting.can_enable_disable = true; -- 是否支持启用停用（支持则在界面上出现勾选框）
+    ydebug_setting.is_enabled_by_default = false; -- 是否默认启用（勾选框默认选中）
+    ydebug_setting.validator = nil; -- 变量值校验函数，检测值除了类型以外的其他合法性（因为带备选值，所以不可能需要校验，不设即可）
+    ydebug_setting.value_width = 120; -- 值显示宽度像素（默认为100）
+end
 -----------------------------------------------------------
 -- 模块脚本
 -----------------------------------------------------------
 function rotation:macro_handler(argument)
     -- 编写执行模块宏对应的处理脚本。
     if (not argument or argument == "") then
-        print("这里应该显示参数说明。");
+        print("使用宏//zkm bingfa baofa控制爆发");
     else
         print("收到宏命令参数：", argument);
     end
